c5e07a0e513428f6511945bf3d92b1e2d82e5d6b
Adrien Barbier <adrien-barbier@hotmail.fr>
2026-02-24 23:17:47 +0100

    go

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,13 +5,8 @@ import { config } from "dotenv";
 config();
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join(__dirname, "src", "lib", "db", "schema.prisma"),
   datasource: {
     url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    url: process.env.DATABASE_URL!,
-    shadowDatabaseUrl: process.env.DIRECT_URL,
   },
 });
